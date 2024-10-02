244f68a8a8b6a7bc5b40e2cbd64c81b405b9c45f
Rosita Castillo R <rositat.castillo@gmail.com>
2024-10-02 17:33:50 -0500

    first commit

---
+++ clase 3 git.R
@@ -0,0 +1,112 @@
+
+#rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental
+$ .
+bash: .: filename argument required
+.: usage: . filename [arguments]
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental
+$ git init
+Initialized empty Git repository in R:/R_Sudio2024-27/Estadistica_Experimental/.git/
+  
+  rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git add.
+git: 'add.' is not a git command. See 'git --help'.
+
+The most similar command is
+add
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git add .
+warning: in the working copy of '.Rproj.user/EF54304F/pcs/debug-breakpoints.pper', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/pcs/files-pane.pper', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/pcs/source-pane.pper', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/pcs/windowlayoutstate.pper', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/pcs/workbench-pane.pper', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/rmd-outputs', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/prop/46422001', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/prop/A5FE9521', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/prop/INDEX', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/session-b22f5c32/B5DF6E06', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/session-b22f5c32/B5DF6E06-contents', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/session-b22f5c32/BA90995F', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/session-b22f5c32/BA90995F-contents', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/EF54304F/sources/session-b22f5c32/C4171DCA', LF will be replaced by CRLF the next time Git touches it
+warning: in the working copy of '.Rproj.user/shared/notebooks/paths', LF will be replaced by CRLF the next time Git touches it
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git commit -m "1er repositorio"
+Author identity unknown
+
+*** Please tell me who you are.
+
+Run
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+to set your account's default identity.
+Omit --global to set the identity only in this repository.
+
+fatal: unable to auto-detect email address (got 'rosit@SITABETH.(none)')
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git config --global user.email "rositat.castillo@gmail.com
+> ^C
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git config --global user.email "rositat.castillo@gmail.com"
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git commit -m "first commit"
+[master (root-commit) 6589ee9] first commit
+ 24 files changed, 323 insertions(+)
+ create mode 100644 .Rproj.user/EF54304F/pcs/debug-breakpoints.pper
+ create mode 100644 .Rproj.user/EF54304F/pcs/files-pane.pper
+ create mode 100644 .Rproj.user/EF54304F/pcs/source-pane.pper
+ create mode 100644 .Rproj.user/EF54304F/pcs/windowlayoutstate.pper
+ create mode 100644 .Rproj.user/EF54304F/pcs/workbench-pane.pper
+ create mode 100644 .Rproj.user/EF54304F/rmd-outputs
+ create mode 100644 .Rproj.user/EF54304F/saved_source_markers
+ create mode 100644 .Rproj.user/EF54304F/sources/prop/46422001
+ create mode 100644 .Rproj.user/EF54304F/sources/prop/A5FE9521
+ create mode 100644 .Rproj.user/EF54304F/sources/prop/INDEX
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/B5DF6E06
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/B5DF6E06-contents
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/BA90995F
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/BA90995F-contents
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/C4171DCA
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/C4171DCA-contents
+ create mode 100644 .Rproj.user/EF54304F/sources/session-b22f5c32/lock_file
+ create mode 100644 .Rproj.user/EF54304F/viewer-cache/6A4F0117.Rdata
+ create mode 100644 .Rproj.user/shared/notebooks/patch-chunk-names
+ create mode 100644 .Rproj.user/shared/notebooks/paths
+ create mode 100644 CLASE 01.R
+ create mode 100644 CLASE IMPORTAR.R
+ create mode 100644 Estadistica_Experimental.Rproj
+ create mode 100644 LA MOLINA 2014 POTATO WUE (FB).xlsx
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ ^C
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ ^C
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ ^C
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ $ git commit -m "first commit"
+bash: $: command not found
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git commit -m "first commit"
+On branch master
+nothing to commit, working tree clean
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$ git commit -m "first commit"
+On branch master
+nothing to commit, working tree clean
+
+rosit@SITABETH MINGW64 /r/R_Sudio2024-27/Estadistica_Experimental (master)
+$
